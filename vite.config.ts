834b840d0c28fbea54729373f7c5754b4a50ecc4
briancode4608 <140543585+briancode4608@users.noreply.github.com>
2025-07-28 13:25:52 +0300

    yes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/oneqana/', // needed for GitHub Pages
+  base: '/oneqana-vivid-showcase/', // needed for GitHub Pages
   server: {
     host: "::",
     port: 8080,
