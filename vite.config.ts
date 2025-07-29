e97f0067803f50e3efc4c8970482245e070ab230
briancode4608 <140543585+briancode4608@users.noreply.github.com>
2025-07-29 12:02:43 +0300

    Prepare for GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/oneqana' : '/',
+  base: mode === 'production' ? '/oneqana/' : '/',
   server: {
     host: "::",
     port: 8080,
